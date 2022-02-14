66670d84e88a8151189a853b97bfcd40ed5e64d9
Paulo Eduardo <pauloedusf@icloud.com>
2022-02-13 21:56:41 -0300

    Adicionando arquivos no package Sobrecarca

---
+++ src/Sobrecarga/Sobrecarga.java
@@ -0,0 +1,23 @@
+package Sobrecarga;
+
+public class Sobrecarga {
+	
+	
+	
+	
+	
+	public static double CalculaQuadrado(double altura, double largura) {
+		return altura * largura;
+	}
+	
+	public static double CalculaRetangulo(double altura, double largura) {
+		return altura * largura;
+	}
+	
+	public static double CalculaTriangulo(double altura, double largura) {
+		return (altura * largura) / 2;
+	}
+	
+	
+
+}
--- src/Sobrecarga/Teste.java
+++ src/Sobrecarga/Teste.java
@@ -3,7 +3,10 @@ package Sobrecarga;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+		Sobrecarga s1 = new Sobrecarga();
+		System.out.println(s1.CalculaQuadrado(5, 5));
+		System.out.println(s1.CalculaRetangulo(5, 5));
+		System.out.println(s1.CalculaTriangulo(5, 5));
 	}
 
 }
